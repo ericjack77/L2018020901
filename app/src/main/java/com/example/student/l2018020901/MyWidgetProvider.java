85133925f4d58e1779baecb34bb3feb9736b33ca
ericchiu <ericjack7@gmail.com>
2018-02-09 13:29:26 +0800

    widget

--- app/src/main/java/com/example/student/l2018020901/MyWidgetProvider.java
+++ app/src/main/java/com/example/student/l2018020901/MyWidgetProvider.java
@@ -1,9 +1,11 @@
 package com.example.student.l2018020901;
 
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.ComponentName;
 import android.content.Context;
+import android.content.Intent;
 import android.widget.RemoteViews;
 
 import java.util.Date;
@@ -20,6 +22,10 @@ public class MyWidgetProvider extends AppWidgetProvider {
                 R.layout.mywidget);
         Date date=new Date();
         remoteViews.setTextViewText(R.id.textView2,date.toString());
+        Intent it= new Intent(context,MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context,0,
+                it,0);
+        remoteViews.setOnClickPendingIntent(R.id.button,pendingIntent);
         ComponentName thisWidget = new ComponentName(context,MyWidgetProvider.class);
         // 取得 Widgets 管理器
         AppWidgetManager manager = AppWidgetManager.getInstance(context);
