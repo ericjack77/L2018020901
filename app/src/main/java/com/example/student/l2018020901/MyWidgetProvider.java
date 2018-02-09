ee43cadd67f8772bc8cbe9c865eb32c98f7063d1
ericchiu <ericjack7@gmail.com>
2018-02-09 11:33:03 +0800

    Initial commit

---
+++ app/src/main/java/com/example/student/l2018020901/MyWidgetProvider.java
@@ -0,0 +1,10 @@
+package com.example.student.l2018020901;
+
+import android.appwidget.AppWidgetProvider;
+
+/**
+ * Created by Student on 2018/2/9.
+ */
+
+public class MyWidgetProvider extends AppWidgetProvider {
+}
